25ff85e02dbcaab2e1f96f948eee6511d67bce34
Eric Smith <eric@esmithy.net>
2019-07-06 15:09:06 -0600

    Removed hard-coded values in favor of cmd line args.

--- onenote-dump/main.py
+++ onenote-dump/main.py
@@ -1,15 +1,23 @@
+import argparse
 import logging
 import os
 import pathlib
+import time
 
 import log
 import onenote_auth
 import onenote
 import pipeline
 
+logger = logging.getLogger()
+
 
 def main():
-    log.setup_logging(logging.DEBUG)
+    args = parse_args()
+    if args.verbose:
+        log.setup_logging(logging.DEBUG)
+    else:
+        log.setup_logging(logging.INFO)
 
     # Allow a redirect URI over plain HTTP (no TLS):
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
@@ -17,20 +25,32 @@ def main():
     # Authorize the app:
     s = onenote_auth.get_session()
 
-    notebook = 'Software Development'
+    output_dir = pathlib.Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    logger.info('Writing to "%s"', output_dir)
 
-    out_dir = pathlib.Path(r'D:\Temp\Notable')
-    out_dir.mkdir(parents=True, exist_ok=True)
-    pipe = pipeline.Pipeline(s, notebook, out_dir)
-
-    pc = 0
-    for page in onenote.get_notebook_pages(s, notebook):
+    start_time = time.perf_counter()
+    pipe = pipeline.Pipeline(s, args.notebook, output_dir)
+    for page_count, page in enumerate(
+        onenote.get_notebook_pages(s, args.notebook), 1
+    ):
+        logger.info('Page: %s', page['title'])
         pipe.add_page(page)
-        pc += 1
-        if pc > 3:
+        if page_count > 3:
             break
 
     pipe.done()
+    stop_time = time.perf_counter()
+    logger.info('Done!')
+    logger.info('%s pages in %.1f seconds', page_count, stop_time - start_time)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('notebook', help='display name of notebook to dump')
+    parser.add_argument('output_dir', help='directory to which to output')
+    parser.add_argument('-v', '--verbose', help='show verbose output')
+    return parser.parse_args()
 
 
 main()
